c430e3dadba2c221c469c69c2387b971673f114f
Sinsry <Sinsry@users.noreply.github.com>
2026-01-24 23:33:12 +0100

    Update

--- noctalia.nix
+++ noctalia.nix
@@ -1,6 +1,14 @@
 { pkgs, inputs, ... }:
 
   {
+
+   # import the nixos module
+    imports = [
+      inputs.noctalia.nixosModules.default
+      ];
+   # enable the systemd service
+    services.noctalia-shell.enable = true;
+
     environment.systemPackages = with pkgs; [
       inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
       ];
