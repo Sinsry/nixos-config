ce6169644657d7c2fd082d9ee083af719541c50b
Sinsry <Sinsry@users.noreply.github.com>
2026-01-24 23:13:21 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, ... }: {
@@ -11,6 +16,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+	./noctalia.nix
       ];
     };
   };
