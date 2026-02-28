ebe1d0a7a4c2aa448da7e7de2f3419c2c37d8b32
Sinsry <113318091+Sinsry@users.noreply.github.com>
2026-02-28 16:09:58 +0100

    refactor(secrets.nix): renommer la variable sinsry en key pour clarté

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,6 @@
 let
-  sinsry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHIB9gJxYTUgrC25g6iRw5L1CBzBnpkigigJzHbKb8B Sinsry@users.noreply.github.com";
+  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHIB9gJxYTUgrC25g6iRw5L1CBzBnpkigigJzHbKb8B";
 in
 {
-  "hosts/valheim/asset/valheim-env.age".publicKeys = [ sinsry ];
+  "hosts/valheim/asset/valheim-env.age".publicKeys = [ key ];
 }
