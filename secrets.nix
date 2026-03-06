43c52ee1f27c7144d82407c7e20ced226f27a05c
Sinsry <113318091+Sinsry@users.noreply.github.com>
2026-03-06 19:51:13 +0100

    refactor: remove unused secrets.nix file

--- secrets.nix
+++
@@ -1,9 +0,0 @@
-let
-  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHIB9gJxYTUgrC25g6iRw5L1CBzBnpkigigJzHbKb8B";
-in
-{
-  "hosts/04-valheim/asset/valheim-env.age".publicKeys = [ key ];
-  "hosts/03-jarvis/asset/transmission-env.age".publicKeys = [ key ];
-  "hosts/03-jarvis/asset/cloudflare-api.age".publicKeys = [ key ];
-  "hosts/03-jarvis/asset/ollama-token.age".publicKeys = [ key ];
-}
