1f0a8b51bad69a57e9c8f8e91ff741863a7e3edb
Sinsry <113318091+Sinsry@users.noreply.github.com>
2026-03-21 02:36:49 +0100

    refactor(common/network-mounts.nix): améliorer les options SMB pour une meilleure compatibilité avec NFS et optimiser le temps d'automountage

--- common/network-mounts.nix
+++ common/network-mounts.nix
@@ -1,19 +1,20 @@
 let
-  nas = "pve";
+  nas = "192.168.1.2";
 
   smbOptions = [
     "rw"
+    "soft"
+    "retrans=2"
+    "echo_interval=1"
+    "nolock"
+    "_netdev"
+    "x-systemd.automount"
+    "x-systemd.mount-timeout=1s"
+    # Options obligatoires pour que SMB fonctionne comme NFS (UID/GID/Inodes)
     "guest"
     "uid=1000"
     "gid=100"
     "noserverino"
-    "noauto"
-    "x-systemd.device-timeout=5s"
-    "x-systemd.mount-timeout=5s"
-    "_netdev"
-    "noatime"
-    "actimeo=0"
-    "cache=none"
     "dir_mode=0777"
     "file_mode=0777"
     "nounix"
